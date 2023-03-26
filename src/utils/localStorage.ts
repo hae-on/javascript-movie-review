1750123b6acd0b0bb05b84fef9a0e623bb716e0a
hae-on <solbi2004@naver.com>
2023-03-26 13:44:01 +0900

    refactor: parse 불가능한 문자열일 경우 에러 처리

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,7 +1,18 @@
 import type { Modal } from "../types/type";
 
-export const getSavedData = (key: string) =>
-  JSON.parse(<string>localStorage.getItem(key)) || [];
+export const getSavedData = (key: string) => {
+  const data = localStorage.getItem(key);
+
+  if (data) {
+    try {
+      return JSON.parse(data);
+    } catch (error) {
+      console.error(`parse 불가능한 문자열입니다: ${error}`);
+    }
+  }
+
+  return [];
+};
 
 export const saveData = (key: string, data: Modal) =>
   localStorage.setItem(key, JSON.stringify(data));
